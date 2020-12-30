2c81e69ed5656e892307236fb4c25825c3ef2890
antoinemadec <ajamadec@gmail.com>
2020-12-30 16:24:44 +0100

    feat: improve API

--- plugin/openrgb.vim
+++ plugin/openrgb.vim
@@ -5,6 +5,49 @@ let g:loaded_openrgb = 'yes'
 
 let g:openrgb_connection_failed = 0
 
-if !exists("g:openrgb_mode_to_color_dict")
-  let g:openrgb_mode_to_color_dict = {'n': "#a89984", 'i': "#83a598", 't': "#b8bb26", 'R': "#8ec07c", 'v': "#fe8019", '': "#fe8019", 'V': "#fe8019"}
+if !exists("g:openrgb_mode_dict")
+  " keys definition
+  let s:keys_arrow =
+        \ ['Key: Right Arrow', 'Key: Left Arrow', 'Key: Up Arrow', 'Key: Down Arrow']
+  let s:keys_special = ['Key: Escape', 'Key: Left Control', 'Key: Left Windows',
+        \ 'Key: Left Alt', 'Key: Left Shift']
+  let s:keys_operator = ['Key: C', 'Key: D', 'Key: Y', 'Key: G']
+  let s:keys_inner_a = ['Key: A', 'Key: I']
+  let s:keys_text_object = ['Key: W', 'Key: P', 'Key: [',  'Key: ]', 'Key: 9', 'Key: 0']
+  " default dict
+  let s:default_dict = {
+    \ 'vim_color': '#00000000',
+    \ 'led_names': [s:keys_arrow + s:keys_special],
+    \ 'led_vim_colors': []
+    \ }
+  let g:openrgb_mode_dict = {
+        \ 'n':       copy(s:default_dict),
+        \ 'i':       copy(s:default_dict),
+        \ 't':       copy(s:default_dict),
+        \ 'R':       copy(s:default_dict),
+        \ 'v':       copy(s:default_dict),
+        \ '':      copy(s:default_dict),
+        \ 'V':       copy(s:default_dict),
+        \ 'default': copy(s:default_dict)
+        \ }
+  " customize dict
+  " -- normal
+  let g:openrgb_mode_dict['n']['vim_color'] = '#a89984'
+  let g:openrgb_mode_dict['n']['led_names'] = [
+        \ s:keys_arrow + s:keys_special,
+        \ s:keys_operator,
+        \ s:keys_inner_a + s:keys_text_object,
+        \ ]
+  " -- insert
+  let g:openrgb_mode_dict['i']['vim_color'] = '#83a598'
+  " -- terminal
+  let g:openrgb_mode_dict['t']['vim_color'] = '#b8bb26'
+  " -- replace
+  let g:openrgb_mode_dict['R']['vim_color'] = '#8ec07c'
+  " -- visual
+  let g:openrgb_mode_dict['v']['vim_color'] = '#fe8019'
+  let g:openrgb_mode_dict['V'] = g:openrgb_mode_dict['v']
+  let g:openrgb_mode_dict[''] = g:openrgb_mode_dict['v']
+  " -- default
+  let g:openrgb_mode_dict['default'] = g:openrgb_mode_dict['n']
 endif
--- rplugin/python3/openrgb_nvim.py
+++ rplugin/python3/openrgb_nvim.py
@@ -1,3 +1,5 @@
+import colorsys
+
 from openrgb import OpenRGBClient
 from openrgb.utils import DeviceType, RGBColor
 import pynvim
@@ -13,9 +15,45 @@ class OpenRGBNvim(object):
         self.device_type = DeviceType.KEYBOARD
         self.is_connected = False
         self.connection_failed = False
+        self.led_names = []
         # vim variables
         self.vim.vars['openrgb_connection_failed'] = False
-        self.mode_to_color_dict = self.vim.vars['openrgb_mode_to_color_dict']
+        self.vim.vars['openrgb_led_names'] = []
+        self.mode_dict = self.vim.vars['openrgb_mode_dict']
+
+    def vim_color_to_rgb(self, vim_color):
+        r = eval("0x" + vim_color[1:3])
+        g = eval("0x" + vim_color[3:5])
+        b = eval("0x" + vim_color[5:7])
+        return (r, g, b)
+
+    def vim_color_to_rgb_color(self, vim_color):
+        return RGBColor(*self.vim_color_to_rgb(vim_color))
+
+    def get_complementary_rgb_colors(self, vim_color, nb_colors):
+        crgbs = []
+        (r, g, b) = self.vim_color_to_rgb(vim_color)
+        (h, s, v) = colorsys.rgb_to_hsv(r, g, b)
+        cs = max(s, 0.7)
+        cv = max(v, 128)
+        for i in range(nb_colors):
+            j = i-1
+            if j < 0:
+                delta = 0
+            else:
+                h_step = 0.25 / (nb_colors//2)
+                sign = (-1, 1)[j % 2]
+                delta = sign*((j//2)+1)*h_step
+            ch = (h+0.5+delta) % 1.0
+            (cr, cg, cb) = (int(x) for x in colorsys.hsv_to_rgb(ch, cs, cv))
+            crgbs.append(RGBColor(cr, cg, cb))
+        return crgbs
+
+    def led_names_to_ids(self, names):
+        ids = []
+        for name in names:
+            ids.append(self.led_names.index(name))
+        return ids
 
     def connect(self):
         if self.connection_failed or self.is_connected:
@@ -23,7 +61,10 @@ class OpenRGBNvim(object):
         try:
             self.client = OpenRGBClient(port=self.port)
             self.device = self.client.get_devices_by_type(self.device_type)[0]
+            self.led_names = [self.device.data.leds[x].name for x in range(
+                len(self.device.data.leds))]
             self.is_connected = True
+            self.vim.vars['openrgb_led_names'] = self.led_names
         except:
             self.connection_failed = True
             self.vim.vars['openrgb_connection_failed'] = True
@@ -33,15 +74,37 @@ class OpenRGBNvim(object):
     @pynvim.function('OpenRGBChangeColor')
     def change_color(self, args):
         vim_color = args[0]
+        led_names = [[]]
+        led_vim_colors = []
+        if len(args) > 1:
+            led_names = args[1]
+        if len(args) > 2:
+            led_vim_colors = args[2]
+        # fill led_rgb_colors
+        led_rgb_colors = []
+        if len(led_names):
+            if len(led_vim_colors):
+                led_rgb_colors = [self.vim_color_to_rgb_color(
+                    vim_color) for vim_color in led_vim_colors]
+            else:
+                # choose led_rgb_colors automatically
+                led_rgb_colors = self.get_complementary_rgb_colors(
+                    vim_color, len(led_names))
         self.connect()
         if self.is_connected:
-            r = eval("0x" + vim_color[1:3])
-            g = eval("0x" + vim_color[3:5])
-            b = eval("0x" + vim_color[5:7])
-            self.device.set_color(RGBColor(r, g, b))
+            # main color
+            rgb_color = self.vim_color_to_rgb_color(vim_color)
+            for c in range(len(self.device.colors)):
+                self.device.colors[c] = rgb_color
+            # led colors
+            for i, rgb_color in enumerate(led_rgb_colors):
+                for c in self.led_names_to_ids(led_names[i]):
+                    self.device.colors[c] = rgb_color
+            # show
+            self.device.show()
 
     @pynvim.function('OpenRGBChangeColorFromMode')
     def change_color_from_mode(self, args):
         vim_mode = args[0]
-        vim_color = self.mode_to_color_dict.get(vim_mode, self.mode_to_color_dict['n'])
-        self.change_color([vim_color])
+        d = self.mode_dict.get(vim_mode, self.mode_dict['default'])
+        self.change_color([d['vim_color'], d['led_names']])
